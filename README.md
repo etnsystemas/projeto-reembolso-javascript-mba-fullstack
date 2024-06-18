7f7b8e345aba7ed1e96e2e6c98d89ad5d2534938
Einstein Xavier <einstein.xavier@gmail.com>
2024-06-18 17:19:00 -0300

    Commit inicial até Obtendo dados do Formulário

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto Reembolso desenvolvido em JavaScript [Figma](https://www.figma.com/design/s8bM0VQXPijdTfhRIP3Nh0/Sistema-de-reembolso-(Community)?node-id=0-1&t=7M05dKoMkLjrKO1f-0)
+
+## Tarabalhando Conceitos Intermediários em JavaScript
---
+++ script.js
@@ -0,0 +1,38 @@
+//Seleciona os elementos do formulário
+const form = document.querySelector('form');
+const amount = document.getElementById('amount');
+const expense = document.getElementById('expense');
+const category = document.getElementById('category');
+
+
+
+amount.oninput = (e) => {
+  let value = e.target.value.replace(/\D/g, "");
+  //Transforma o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,50)
+  value = Number(value) / 100
+  amount.value = formatCurrencyBRL(value);
+}
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+
+  console.log(newExpense);
+
+}
